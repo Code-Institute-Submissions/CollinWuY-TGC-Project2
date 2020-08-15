30a7c82c163c32bf797a38130e80aee79c98ded0
Collin Wu <collinwu.y@gmail.com>
2020-08-15 14:07:51 +0800

    OneMap editing

--- js/main.js
+++ js/main.js
@@ -4,5 +4,23 @@ $(document).ready(function() {
 
     $('#user-input').on('click', function() {
         navigator.geolocation.getCurrentPosition(successCallBack, errorCallBack);
+
+        const onMapParams = new URLSearchParams({
+            searchVal: 'URA',
+            returnGeom: 'Y',
+            getAddrDetails: 'Y'
+        })
+        const oneMapURL = `https://developers.onemap.sg/commonapi/search?${onMapParams.toString()}`;
+
+        fetch(oneMapURL)
+            .then(response => response.json())
+            .then(data => mapLong = data.results[0].LONGTITUDE)
+            .then(() => console.log(mapLong));
+
+        fetch(oneMapURL)
+            .then(response => response.json())
+            .then(data => mapLat = data.results[0].LATITUDE)
+            .then(() => console.log(mapLat));
+
     })
 })
